99e93c43b9e7f360f041d982a530801d034bb479
Mariana <maryana.oleksandrovna@gmail.com>
2021-04-13 01:33:10 +0300

    added HW10

---
+++ homework10/main.js
@@ -0,0 +1,32 @@
+
+function generateBlocks() {
+    const square = document.body.insertAdjacentHTML('afterbegin', '<div style = "width:250px;">' +
+    '<div class="square"></div>'.repeat(25) + '</div>');
+
+    document.querySelectorAll('.square').forEach(element => {
+        element.style.backgroundColor = '#' + Math.random().toString(16).slice(2,8);
+        element.style.height = "50px";
+        element.style.width = "50px";
+        element.style.display = "inline-block";
+        element.style.float = "left";
+    });
+};
+
+//Advanced
+function generateBlocksInterval() {
+    const square = document.body.insertAdjacentHTML('afterbegin', '<div style = "width:250px;">' +
+    '<div class="square"></div>'.repeat(25) + '</div>');
+
+    setInterval(() => {
+        document.querySelectorAll('.square').forEach(element => {
+            element.style.backgroundColor = '#' + Math.random().toString(16).slice(2,8);
+            element.style.height = "50px";
+            element.style.width = "50px";
+            element.style.display = "inline-block";
+            element.style.float = "left";
+        });
+    },1000);
+
+};
+
+generateBlocksInterval();
