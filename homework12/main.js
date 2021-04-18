66164c5450f320d58650c3aa8a2168aa45493f75
Mariana <maryana.oleksandrovna@gmail.com>
2021-04-18 16:29:22 +0300

    changed

--- homework12/main.js
+++ homework12/main.js
@@ -1,15 +1,16 @@
 async function getRandomChinese(length) {
-    let result = ''; 
+    let result ='';
+
     for(let i = 0; i < length; i++) {
         await new Promise(resolve => {
-            setTimeout(() => {
-            const sign = Date.now().toString().slice(-5);
-            result += String.fromCharCode(sign);
-            resolve(result)
-            }, 50)
-        })
+            setTimeout (() => {
+                const sign = Date.now().toString().slice(-5);
+                result += String.fromCharCode(sign);
+                resolve(result);
+            }, 50);
+        });
     }
-    console.log(result);    
+    console.log(result);
 }
 
-getRandomChinese(4);
+getRandomChinese(4).then(() => getRandomChinese(16));
